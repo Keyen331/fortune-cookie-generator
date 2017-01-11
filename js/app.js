1812bf0ed7ca40512e182dfa7869880e102e0319
Keyen331 <banayos.kay@gmail.com>
2017-01-11 08:45:48 -1000

    updating

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-document.getElementById('fortune-cookie-text')
+function hello() {
+  document.getElementById('fortune-cookie-text').innerHTML = fortunelist[0];
+  document.getElementById('previous-fortunes').innerHTML = "hahahaha";
+}
 
 var fortunelist = ["You'll die", "He's coming", "Enjoy it now", "Bye bye"];
 
